c788f8678fd410a155661f4d8a7815eeebb899e7
fajri-blin <fajriindonesia1945@gmail.com>
2023-06-26 16:57:50 +0700

    test

--- API/Contracts/IUniversityRepository.cs
+++ API/Contracts/IUniversityRepository.cs
@@ -4,13 +4,10 @@ namespace API.Contracts;
 
 public interface IUniversityRepository
 {
-<<<<<<< HEAD
     IEnumerable<University> GetByName(string name);
-=======
     ICollection<University> GetAll();
     University? GetByGuid(Guid guid);
     University Create(University university);
     bool Update(University university);
     bool Delete(Guid guid);
->>>>>>> parent of ecb12b2 (Refactoring)
 }
